c97bcb3edb08a3b554a1a1d1e66f04a8f323cf08
Unrealrojo234 <otienoryan812@gmail.com>
2024-07-16 06:08:58 -0700

    Made the staff page

---
+++ staff.js
@@ -0,0 +1,9 @@
+$('body').css('display','none')
+$('.loading').css('display','block')
+
+
+$(document).ready(function() {
+    // Runs after the DOM is loaded.
+    $('body').css('display','block')
+    $('.loading').css('display','none')
+  });
